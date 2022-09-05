001e9eb842400f62c5629845f55e55601b97ce5a
ekaterina-demeshko-idf <ekaterina.demeshko@idfinance.com>
2022-09-05 12:27:33 +0300

    add injector in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:presentation/my_app.dart';
+import 'di/app_injector.dart';
 
 void main() {
+  initAppInjector();
   runApp(const MyApp());
 }
