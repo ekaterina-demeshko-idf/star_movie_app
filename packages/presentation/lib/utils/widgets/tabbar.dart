0ea69a346c907312c9d5665753560c2df4c7d6a0
ekaterina-demeshko-idf <ekaterina.demeshko@idfinance.com>
2022-09-07 11:56:27 +0300

    fix split tabbar widget

--- packages/presentation/lib/utils/widgets/tabbar.dart
+++ packages/presentation/lib/utils/widgets/tabbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:presentation/utils/colors.dart';
+import 'package:presentation/utils/widgets/tabbarHomeScreen.dart';
 
 class TabBarContainer extends StatelessWidget {
   const TabBarContainer({
@@ -37,45 +38,6 @@ class TabBarContainer extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class TabBarHomeScreen extends StatelessWidget {
-  const TabBarHomeScreen({
-    Key? key,
-    required this.tabController,
-  }) : super(key: key);
-
-  final TabController tabController;
-
-  @override
-  Widget build(BuildContext context) {
-    return TabBar(
-      unselectedLabelColor: Colors.grey,
-      labelColor: Colors.white,
-      indicatorColor: Colors.red,
-      indicatorWeight: 2,
-      indicator: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      controller: tabController,
-      tabs: [
-        Tab(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Icon(Icons.play_circle_fill_rounded),
-              SizedBox(width: 8),
-              Text('Now Showing'),
-            ],
-          ),
-        ),
-        const Tab(
-          text: 'Coming Soon',
-        ),
-      ],
     );
   }
 }
