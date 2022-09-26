068246ea78dc3368b0acf05529c62e342015de36
ekaterina-demeshko-idf <ekaterina.demeshko@idfinance.com>
2022-09-26 13:27:05 +0300

    hide apikey

--- packages/domain/lib/utils/getSecrets.dart
+++ packages/domain/lib/utils/getSecrets.dart
@@ -4,6 +4,5 @@ import "package:yaml/yaml.dart";
 Future<YamlMap> getSecrets() async {
   final data = await s.rootBundle.loadString('secrets.yaml');
   final mapData = loadYaml(data);
-  print('GET SECRETS $mapData');
   return mapData;
 }
